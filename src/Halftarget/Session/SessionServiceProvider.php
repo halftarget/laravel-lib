c0147b0e93d266f212637b368a532da31bc940fd
Sebastian Arrubia <sarrubia@gmail.com>
2015-04-01 22:25:29 -0300

    Adding Session Provider using Header value instead of Cookies

---
+++ src/Halftarget/Session/SessionServiceProvider.php
@@ -0,0 +1,57 @@
+<?php namespace  Halftarget\Session;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Session\SessionManager;
+
+class SessionServiceProvider extends ServiceProvider {
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerSessionManager();
+
+        $this->registerSessionDriver();
+
+        $this->app->singleton('Halftarget\Session\Middleware\StartSession');
+    }
+
+    /**
+     * Register the session manager instance.
+     *
+     * @return void
+     */
+    protected function registerSessionManager()
+    {
+        $this->app->singleton('session', function($app)
+        {
+            return new SessionManager($app);
+        });
+    }
+
+    /**
+     * Register the session driver instance.
+     *
+     * @return void
+     */
+    protected function registerSessionDriver()
+    {
+        $this->app->singleton('session.store', function($app)
+        {
+            // First, we will create the session manager which is responsible for the
+            // creation of the various session drivers when they are needed by the
+            // application instance, and will resolve them on a lazy load basis.
+            $manager = $app['session'];
+
+            return $manager->driver();
+        });
+    }
+
+
+
+
+} 
